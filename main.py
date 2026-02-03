cf444aad9fa97f9bed9f9efc38a234610c0f3ddd
SlimyTheMoon <marvin.services20@pm.me>
2026-02-03 22:09:41 +0100

    feat(bot): add profanity filter and health tracker
    Implements message moderation and health tracking command
    Configures logging and environment variables

--- main.py
+++ main.py
@@ -5,3 +5,31 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+API_URL_BASES_GOODS = os.getenv("API_URL_BASES_GOODS")
+API_URL_BASES_STATS = os.getenv("API_URL_BASES_STATS")
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+print("API_URL_BASES_GOODS:", API_URL_BASES_GOODS)
+print("API_URL_BASES_STATS:", API_URL_BASES_STATS)
+bot = commands.Bot(command_prefix='?', intents=intents)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if 'shit' in message.content.lower():
+        await message.delete()
+        await message.channel.send(f"{message.author.mention}, please watch your language.")
+    await bot.process_commands(message)
+
+@bot.command()
+async def healthtracker(ctx):
+    await ctx.send(f"Test")   
+
+
+bot.run(token, log_handler=handler, log_level=logging.DEBUG)
